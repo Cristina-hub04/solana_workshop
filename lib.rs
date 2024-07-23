55f8bd9bd2f370e3d9abea45ef0ca3594366e00d
Cristina-hub04 <ioja.cristinag@gmail.com>
2024-07-23 19:39:23 +0300

    Add files via upload

---
+++ lib.rs
@@ -0,0 +1,42 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("42VqbAT69VDizZunoe6FASUQUnncPNkLPCr1xgyxh224");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+        instructions::take_offer::withdraw_and_close_vault(context)
+    pub mod escrow {
+        use super::*;
+        pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+        ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, 
+        token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+        instructions::take_offer::withdraw_and_close_vault(context)
+ }
+   }
+}
+    }
+    }
+}
---
+++ mod.rs
@@ -0,0 +1,8 @@
+pub mod offer;
+pub use offer::*;
+
+pub mod make_offer;
+pub use make_offer::*;
+
+pub mod take_offer;
+pub use take_offer::*;
