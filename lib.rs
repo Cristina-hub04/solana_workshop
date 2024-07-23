3dfbd19fd0720c60f312653b9885a9e2c3d76e27
Cristina-hub04 <ioja.cristinag@gmail.com>
2024-07-23 19:53:24 +0300

    Add files via upload

--- lib.rs
+++ lib.rs
@@ -11,32 +11,56 @@ pub use state::*;
 
 declare_id!("42VqbAT69VDizZunoe6FASUQUnncPNkLPCr1xgyxh224");
 
+[19:45, 23/07/2024] Cristina: pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+[19:46, 23/07/2024] Cristina: // A 'mod' is a Rust module. But the #[program] makes it into a Solana program! 
 #[program]
-pub mod escrow {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
-    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
-        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
-        instructions::take_offer::withdraw_and_close_vault(context)
-    pub mod escrow {
-        use super::*;
-        pub fn make_offer(
-        context: Context<MakeOffer>,
-        id: u64,
-        token_a_offered_amount: u64,
-        token_b_wanted_amount: u64,
-        ) -> Result<()> {
-        instructions::make_offer::send_offered_tokens_to_vault(&context, 
-        token_a_offered_amount)?;
-        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
-    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
-        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
-        instructions::take_offer::withdraw_and_close_vault(context)
+pub mod favorites {
+ use super::*;
+ // Our instruction handler! It sets the user's favorite number and color
+ pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies: 
+Vec<String>) -> Result<()> {
+ let user_public_key = context.accounts.user.key();
+ msg!("Greetings from {}", context.program_id);
+ msg!(
+ "User {user_public_key}'s favorite number is {number}, favorite color is: {color}"
+,
+ );
+ msg!(
+ "User's hobbies are: {:?}"
+,
+ hobbies
+ ); 
+ context.accounts.favorites.set_inner(Favorites {
+ number,
+ color,
+ hobbies
+ });
+ Ok(())
  }
-   }
+ // We can also add a get_favorites instruction handler to return the user's favorite number and color
 }
-    }
-    }
+[19:46, 23/07/2024] Cristina: // What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+ pub number: u64,
+ #[max_len(50)]
+ pub color: String,
+ #[max_len(5, 50)]
+ pub hobbies: Vec<String>
+}
+[19:46, 23/07/2024] Cristina: // When people call the set_favorites instruction, they will need to provide the 
+accounts that will be modifed. This keeps Solana fast!
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+ #[account(mut)]
+ pub user: Signer<'info>,
+ #[account(
+ init, 
+ payer = user, 
+ space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+ seeds=[b"favorites", user.key().as_ref()],
+ bump)]
+ pub favorites: Account<'info, Favorites>,
+ pub system_program: Program<'info, System>,
 }
